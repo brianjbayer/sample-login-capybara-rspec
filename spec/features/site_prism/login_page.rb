289ce1d01b7f16f7b055c707e786da791caaae9c
brianjbayer <brianjbayer@gmail.com>
2016-08-07 20:01:45 -0400

    cleanup

--- spec/features/site_prism/login_page.rb
+++ spec/features/site_prism/login_page.rb
@@ -1,24 +1,3 @@
-=begin
-class SearchPage < SitePrism::Page
-  set_url "http://www.google.com"
-
-  element :search_field, "input[name='q']"
-  element :google_search, "button[value='Search']"
-  element :logo, '#hplogo'
-
-  def search_for(text)
-    search_field.set text
-    google_search.click if ENV['HEADLESS'].nil? #terrible hack not worth spending time on
-  end
-end
-
-        loginFormUsernameInput { loginForm.$("input[name=username]") }
-        loginFormPasswordInput { loginForm.$("input[name=password]") }
-        loginFormSubmitButton  { loginForm.$("button[type=submit]") }
-
-=end
-
-
 class LoginPage < SitePrism::Page
   set_url "http://the-internet.herokuapp.com/login"
 
