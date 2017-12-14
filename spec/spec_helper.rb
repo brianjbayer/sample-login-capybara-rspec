ac95e34c66cf28e874ac5f3350faff16f5ad9ca6
brianjbayer <brianjbayer@gmail.com>
2017-12-13 21:36:14 -0500

    oh, safari

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,24 +5,36 @@ require 'capybara/poltergeist'
 require 'selenium/webdriver'
 
 ### METHODS ###
-def register_vanilla_browser(selenium_browser_name)
-  puts "register_vanilla_browser(#{selenium_browser_name})"
+def register_standard_browser(selenium_browser_name)
   Capybara.register_driver selenium_browser_name do |app|
     Capybara::Selenium::Driver.new(app, browser: selenium_browser_name)
   end
-  selenium_browser_name
 end
 
 def configure_driver(registered_driver)
   # Assumption: registered_driver supports javascript and non-rack
   Capybara.configure do |c|
+    c.javascript_driver     = registered_driver
     c.default_driver        = registered_driver
     c.run_server            = false
     c.default_max_wait_time = 15
   end
 end
 
+def configure_safari
+  # Safari can NOT be set as the default browser - the choice here is arbitrary
+  Capybara.configure do |c|
+    c.javascript_driver     = :safari
+    c.default_driver        = :selenium_chrome_headless
+    c.run_server            = false
+    c.default_max_wait_time = 15
+  end
+  Capybara.current_driver = :safari
+end
+
 ### MAIN ###
+
+## Set Browser ##
 # Set the specific browser from environment variable or not (default is :selenium)
 # This uses some selenium/webdriver "inside baseball", specifically that
 # :selenium_chrome, :selenium_chrome_headless, and :poltergeist is already registered
@@ -35,27 +47,27 @@ when 'chrome_headless', 'headless_chrome'
   configure_driver(:selenium_chrome_headless)
 
 when 'firefox'
-  register_vanilla_browser(:firefox)
+  register_standard_browser(:firefox)
   configure_driver(:firefox)
 
 when 'phantomjs'
   configure_driver(:poltergeist)
 
 when 'safari'
-  register_vanilla_browser(:safari)
-  configure_driver(:safari)
+  register_standard_browser(:safari)
+  configure_safari
 
 else
   STDERR.puts '>> USING DEFAULT DRIVER (:selenium) <<'
   configure_driver(:selenium)
 end
 
-# Configure Page Object
+## Configure Page Object ##
 SitePrism.configure do |config|
   config.use_implicit_waits = true
 end
 
-# Configure Test Framework
+## Configure Test Framework ##
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
