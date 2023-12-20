6aa7262b09a268887d7478e6d0cd7f472440459c
brianjbayer <brianjbayer@gmail.com>
2023-12-19 22:38:02 -0500

    Invalid password test

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -17,4 +17,11 @@ feature 'User logs in' do
     # Allow at least 5 seconds for page to load
     expect(secure_area_page).to be_displayed(5)
   end
+
+  scenario 'with invalid password and an error is displayed' do
+    login_page.login_with_invalid_password
+    expect(login_page.current_url).to eq(login_page.url)
+    expect(login_page).to have_login_error
+    expect(login_page.login_error.text).to include('Your password is invalid!')
+  end
 end
--- spec/features/site_prism/login_page.rb
+++ spec/features/site_prism/login_page.rb
@@ -7,9 +7,17 @@ class LoginPage < SitePrism::Page
   element :password_input, 'input[id="password"]'
   element :submit_button, 'button[type="submit"]'
 
+  element :login_error, '#flash'
+
   def login_with_valid_credentials
     username_input.set valid_login_username
     password_input.set valid_login_password
+    submit_button.click
+  end
+
+  def login_with_invalid_password
+    username_input.set valid_login_username
+    password_input.set 'NotAValidPassword'
     submit_button.click
   end
 end
